26990b1623eb784d86c3cceefca93cc747ac0d34
Ms.Ayesha <ms.ayshraj175@gmail.com>
2026-01-09 11:22:48 +0500

    fix validation

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const dotenv = require("dotenv");
-dotenv.config();
-const app = require("./app");
 const connectDB = require("./config/db");
-
+dotenv.config();
 connectDB();
+
+const app = require("./app");
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is Running at http://localhost:${PORT}`));
