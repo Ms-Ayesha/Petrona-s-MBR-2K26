02873049258dd66e1ee0485866deafc452ac2284
Ms.Ayesha <ms.ayshraj175@gmail.com>
2026-02-10 01:12:38 +0500

    credentals

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,8 @@
 const nodemailer = require("nodemailer");
-
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: process.env.SMTP_PORT == 465,
+  port: process.env.SMTP_PORT,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,22 +1,39 @@
+// src/utils/sendEmail.js
+const nodemailer = require("nodemailer");
 const fs = require("fs");
 const path = require("path");
-const transporter = require("../config/mail"); // ✅ correct path
 
 const sendEmail = async (to, subject, templateName, variables = {}) => {
+  // Load the HTML template
   const templatePath = path.join(__dirname, "../Template", templateName);
-
   let html = fs.readFileSync(templatePath, "utf8");
 
+  // Replace variables in template
   Object.keys(variables).forEach((key) => {
-    html = html.replace(new RegExp(`{{${key}}}`, "g"), variables[key]);
+    const regex = new RegExp(`{{${key}}}`, "g");
+    html = html.replace(regex, variables[key]);
   });
 
+  // SMTP transporter (like sendPdfMail)
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,      // SMTP host
+    port: process.env.SMTP_PORT,      // SMTP port
+    secure: true,                      // true for port 465, false for 587
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  // Send the email
   await transporter.sendMail({
-    from: `"MBR Team" <${process.env.EMAIL_USER}>`,
+    from: `"MBR Platform" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html,
   });
+
+  console.log("Email sent successfully via SMTP");
 };
 
 module.exports = sendEmail;
