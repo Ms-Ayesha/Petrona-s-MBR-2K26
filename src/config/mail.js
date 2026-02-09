6767be1534691f27c3e2673ff4ecfd2ab0feff89
Ms.Ayesha <ms.ayshraj175@gmail.com>
2026-02-10 01:22:52 +0500

    credentals

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,11 +1,15 @@
 const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: true,
+  host: process.env.SMTP_HOST,      // mail.petronasmbr.com
+  port: Number(process.env.SMTP_PORT), // 465
+  secure: true,                      // SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,       // optional, kabhi SSL cert problem ke liye
   },
 });
 
