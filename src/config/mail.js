53107ac10be378155cbcfa304b896b5ad3857532
Ms.Ayesha <ms.ayshraj175@gmail.com>
2026-02-13 21:06:46 +0500

    cpanel fixed

--- src/config/mail.js
+++ src/config/mail.js
@@ -2,12 +2,14 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,   // your mail server
-  port: 465,
+  port: Number(process.env.SMTP_PORT),
   secure: true, // TRUE for 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+   logger: true,
+  debug: true,
 });
 
 
